f141de2ad8070b812f9ab08dec2b68447c242537
chimalexoficial <you@example.com>
2021-06-26 16:44:17 -0500

    Projects Component done

--- src/components/Projects.js
+++ src/components/Projects.js
@@ -1,32 +1,37 @@
 import React from 'react';
 import sites from '../data/sites.json';
+import Button from 'react-bootstrap/Button';
+
 
 export const Projects = (() => {
     return (
         <>
-            <h2 className="mb-5">Projects</h2>
-            {
-                sites.map(site => <div className="container-fluid p-0">
-                    <section className="resume-section" id="experience">
-                        <div className="resume-section-content">
-                            <div className="d-flex flex-column flex-md-row justify-content-between mb-5">
-                                <div className="flex-grow-1">
-                                    <h3 className="mb-0">{site.title}</h3>
-                                    <div className="subheading mb-3"><a href={site.domain}>{site.domain}</a></div>
-                                    <p>{site.description}</p>
-                                </div>
-                                <div className="container">
-                                    <img className="img-sites" src={site.imgPreview} alt="Website" />
+            <div id="projects">
+                <h2 className="mb-5">Projects</h2>
+                {
+                    sites.map(site => <div key={site.title} className="container-fluid p-0">
+                        <section className="resume-section">
+                            <div className="resume-section-content">
+                                <div className="d-flex flex-column flex-md-row justify-content-between mb-5">
+                                    <div className="flex-grow-1">
+                                        <h3 className="mb-0">{site.title}</h3>
+                                        <div className="subheading mb-3">Technology: {site.technology}</div>
+                                        <p>{site.description}</p>
+                                        <p><strong>Client from: </strong>{site.clientFrom}</p>
+                                        <p><strong>Status: </strong>{site.status}</p>
+                                        <p id="finished"><strong>Finished: </strong>{site.finished}</p>
+                                        <Button variant="primary" href={site.domain}>VISIT WEBSITE</Button>
+                                    </div>
+                                    <div className="container shadow-strong p-3 rounded">
+                                        <img className="img-sites" src={site.imgPreview} alt="Website" />
+                                    </div>
                                 </div>
                             </div>
-
-
-
-                        </div>
-                    </section>
-                </div>)
-            }
-
+                        </section>
+                        <hr />
+                    </div>)
+                }
+            </div>
         </>
     )
 })
